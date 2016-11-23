c3c1aca9a32325ef91fdedd9371c1254f85f7129
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-11-23 14:35:50 +0100

    adds conversion of numpy array into mne data

--- functions/read_eeg.py
+++ functions/read_eeg.py
@@ -1,7 +1,17 @@
 import numpy as np
 import h5py
+import mne
 
 def read_mat(path):
     mat = h5py.File(path)
     data = mat['eeg'][()] #stores is as a numpy array - [()]
     return(data)
+
+def numpy_mne(data, frequency):
+    n_channels = data.shape[0]
+    ch_names = list(range(1, n_channels + 1))
+    ch_names = [str(ch) for ch in ch_names]
+    ch_types = ["seeg"] * n_channels
+    info = mne.create_info(ch_names = ch_names, sfreq = frequency, ch_types = ch_types)
+    raw = mne.io.RawArray(data, info)
+    return(raw)
