7e8a7af54af5703bdfc27dbae0e3af7ebda601bb
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-11-28 18:14:56 +0100

    adds the new functions to main process

--- mne_test.py
+++ mne_test.py
@@ -1,26 +1,37 @@
 import mne
+import numpy as np
 
-from functions import read_eeg as eegrd
-from functions import read_experiment_data as exprd
+from functions import mne_prepping as mneprep
+from functions import mne_helpers as mnehelp
+from mne.time_frequency import tfr_multitaper, tfr_stockwell, tfr_morlet
+
 path = "U:\\OneDrive\\FGU\\iEEG\\p83\\UTAlloEgo\\EEG\\Preprocessed\\prep_bipolar_250.mat"
+path = "D:\\IntracranialElectrodes\\Data\\p83\\UTAlloEgo\\EEG\\Preprocessed\\prep_perHeadbox_250.mat"
+path = "D:\\IntracranialElectrodes\\Data\\p83\\v\\EEG\\Preprocessed\\prep_perElectrode_250.mat"
+path = "D:\\IntracranialElectrodes\\Data\\p83\\UTAlloEgo\\EEG\\Preprocessed\\prep_250.mat"
 
 FREQUENCY = 250
 
 path_events = "U:\\OneDrive\\FGU\\iEEG\\p83\\UTAlloEgo\\EEG\\Preprocessed\\p83_UT.csv"
-data = eegrd.read_mat(path)
+path_events = "D:/IntracranialElectrodes/Data/p83/UTAlloEgo/experiment_data/p83_ut.csv"
 
-raw = eegrd.numpy_mne(data, FREQUENCY)
-pd_events = exprd.read_events(path_events)
-mne_events, mapp = exprd.mne_epochs_from_pd(pd_events, FREQUENCY)
+raw = mneprep.load_raw(path)
+mne_events, mapp = mneprep.load_events(path_events)
 
 raw.plot(events = mne_events, scalings = 'auto')
 
-
 epochs = mne.Epochs(raw, mne_events, event_id = mapp, tmin=-0.5, tmax=2, add_eeg_ref = False)
-epochs.plot(block=True, scalings='auto')
+epochs.plot(block = True, scalings = 'auto')
 
 onsets = epochs['onsets_500_1500']
 onsets.plot(scalings = 'auto', n_epochs = 5)
 
 stops = epochs['stops_500_1500']
 stops.plot(scalings = 'auto', n_epochs = 5)
+
+freqs = np.arange(2, 10, 1)
+n_cycles = freqs / 2
+
+picks = mnehelp.def_picks(raw)
+power = tfr_morlet(onsets, freqs=freqs, n_cycles=n_cycles, picks = picks, return_itc=False)
+power.plot()
