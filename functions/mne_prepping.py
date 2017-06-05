f8e7aa48bdc4ec4967ff27e16ef9778169ddf165
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-06-05 17:19:21 +0200

    Redoes hwo events are loaded

--- functions/mne_prepping.py
+++ functions/mne_prepping.py
@@ -7,7 +7,25 @@ def load_raw(data_path, frequency):
     raw = eegrd.numpy_mne(data, frequency)
     return raw
 
-def load_events(events_path, frequency):
+
+def load_matlab_events(events_path):
     pd_events = exprd.read_events(events_path)
+    pd_events = pd_events.drop(['Unnamed: 2'], 1)
+    return(pd_events)
+
+def load_unity_events(events_path):
+    pd_events = exprd.read_events(events_path)
+    pd_events = pd_events.drop(['trialIDs','Unnamed: 8'], 1)
+    pd_events = pd_events.melt(id_vars = ['type'])
+    pd_events['name'] = pd_events['variable'] + '_' + pd_events['type']
+    pd_events = pd_events.drop(['type', 'variable'], 1)
+    pd_events = pd_events.rename(index=str, columns={"value": "time"})
+    return pd_events
+
+def pd_to_mne_events(pd_events, frequency):
     mne_events, mapp = exprd.mne_epochs_from_pd(pd_events, frequency)
     return mne_events, mapp
+
+def create_mne_events(events_path, frequency):
+    pd_events = load_matlab_events(events_path)
+    return pd_to_mne_events(pd_events, frequency)
