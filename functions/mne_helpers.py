6e43c607f9a2a5de9a128c3dee0eb017f3e3e3f1
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-06-06 16:40:59 +0200

    defines new picking functions

--- functions/mne_helpers.py
+++ functions/mne_helpers.py
@@ -2,9 +2,17 @@ import mne
 from mne.channels.layout import Layout
 import numpy as np
 
-def def_picks(raw):
+def picks_all_localised(raw, pd_montage, where):
+    all_channels = picks_all(raw)
+    where_channels = picks_localised(pd_montage, where)
+    return np.intersect1d(all_channels, where_channels)
+
+def picks_all(raw, where = None, pd_montage = None):
     picks = mne.pick_types(raw.info, seeg = True, meg = True, eeg = False, stim = False, eog = True, exclude ='bads')
     return picks
+
+def picks_localised(pd_montage, name):
+    return pd_montage[name == pd_montage.headboxNumber].index
 
 def custom_box_layout(picks):
     box = (-.1, 1.1, -.1, 1.1)
@@ -17,3 +25,4 @@ def custom_box_layout(picks):
     ch_indices = range(len(picks))
     box_layout = Layout(box, pos, ch_names, ch_indices, 'box')
     return(box_layout)
+
