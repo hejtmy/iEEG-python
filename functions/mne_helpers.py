28396f66448db7e3c32a11a98d076989c1ca5e2e
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-06-07 15:17:03 +0200

    redefines plotting box

--- functions/mne_helpers.py
+++ functions/mne_helpers.py
@@ -14,15 +14,20 @@ def picks_all(raw, where = None, pd_montage = None):
 def picks_localised(pd_montage, name):
     return pd_montage[name == pd_montage.headboxNumber].index
 
-def custom_box_layout(picks):
+# Creates box layout for non standard topological images (such as seeg)
+def custom_box_layout(picks, ncol = 6):
+    # Creates boundaries of the 0-1 box, will be
     box = (-.1, 1.1, -.1, 1.1)
+    # just puts the channel names to strings
     ch_names = ["{:d}".format(x) for x in picks]
-    x, y = np.mgrid[0:1:0.1, 0:1:0.1]
+    nrow = np.ceil(len(picks) / ncol)
+    x, y = np.mgrid[0:1:(1/nrow), 0:1:(1/ncol)]
     xy = np.vstack([x.ravel(), y.ravel()]).T
-    w, h = [.05, .05]
+    w, h = [(1/(2 * ncol)), 1/(2 * nrow)]
     w, h = [np.tile(i, xy.shape[0]) for i in [w, h]]
     pos = np.hstack([xy, w[:, None], h[:, None]])
+    # removes
+    pos = pos[:len(picks)]
     ch_indices = range(len(picks))
     box_layout = Layout(box, pos, ch_names, ch_indices, 'box')
     return(box_layout)
-
