9b8b9d9bfe613fd525ffef6770bed08fd5639c28
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-06-13 18:07:20 +0200

    p136 change

--- p136_Unity.py
+++ p136_Unity.py
@@ -12,8 +12,8 @@ from mne.time_frequency import psd_multitaper
 
 from mne.time_frequency import tfr_multitaper, tfr_stockwell, tfr_morlet
 
-#base_path = "D:\\IntracranialElectrodes\\Data\\p136\\UnityAlloEgo\\EEG\\Preprocessed\\"
-base_path = "U:\\OneDrive\\FGU\\iEEG\\p136\\"
+base_path = "D:\\IntracranialElectrodes\\Data\\p136\\"
+#base_path = "U:\\OneDrive\\FGU\\iEEG\\p136\\"
 
 path_original_vr = base_path + "UnityAlloEgo\\EEG\\Preprocessed\\prep_256.mat"
 path_perhead_vr = base_path + "UnityAlloEgo\\EEG\\Preprocessed\\prep_perHeadbox_256.mat"
@@ -27,7 +27,7 @@ FREQUENCY = 256
 
 # Loading Unnity data
 raw_original_vr = mneprep.load_raw(path_original_vr, FREQUENCY)
-#raw_perhead_vr = mneprep.load_raw(path_perhead_vr, FREQUENCY)
+raw_perhead_vr = mneprep.load_raw(path_perhead_vr, FREQUENCY)
 #raw_bip_vr = mneprep.load_raw(path_bip_vr, FREQUENCY)
 
 pd_unity_events = mneprep.load_unity_events(path_unity_events)
@@ -39,41 +39,44 @@ pd_events = mneprep.clear_pd(pd_events)
 pd_montage = readeegr.read_montage(path_montage)
 mne_events_vr, mapp_vr = mneprep.pd_to_mne_events(pd_events, FREQUENCY)
 
+##PLOTS
 raw_original_vr.plot(events = mne_events_vr, scalings='auto')
-#raw_perhead_vr.plot(events = mne_events_vr, scalings='auto')
+raw_perhead_vr.plot(events = mne_events_vr, scalings='auto')
 raw_original_vr.info["bads"] = ['SEEG_47']
 
 ## Epoching
 epochs_original_vr = mne.Epochs(raw_original_vr, mne_events_vr, event_id=mapp_vr, tmin=-3, tmax=3, add_eeg_ref=False)
 
-epochs_original_vr['onsets_500_1500', 'stops_500_1500'].plot(block=True, scalings='auto')
-
-epochs_original_vr["pointingEnded_Ego", "pointingEnded_Allo"].plot(block=True, scalings='auto')
-
+# TIME FREQ
 freqs = np.arange(2, 30, 1)
 n_cycles = freqs / 2
 
-pick_orig_hip = mnehelp.picks_all_localised(epochs_original_vr, pd_montage, 'Hi')
+pick_orig_hip = mnehelp.picks_all_localised(raw_original_vr, pd_montage, 'Hi')
 raw_original_vr.plot_psd(picks=pick_orig_hip)
 
-box = mnehelp.custom_box_layout(pick_orig_hip, 5)
-plot_pick_orig_hip = range(0, len(pick_orig_hip))
+pick_orig_hip_names = mne.pick_info(raw_original_vr.info, pick_orig_hip)['ch_names']
+box = mnehelp.custom_box_layout(pick_orig_hip_names, 3)
+plot_pick_orig_hip = range(len(pick_orig_hip))
+#PLOTS
+epochs_original_vr['onsets_500_1500', 'stops_500_1500'].plot(picks = pick_orig_hip, block=True, scalings='auto')
+epochs_original_vr["pointingEnded_Ego", "pointingEnded_Allo"].plot(block=True, scalings='auto')
 
 power_point_orig_hip_vr_ego = tfr_morlet(epochs_original_vr['pointingEnded_Ego'], freqs=freqs, n_cycles=n_cycles,
                                     picks = pick_orig_hip, return_itc=False)
 power_point_orig_hip_vr_allo = tfr_morlet(epochs_original_vr['pointingEnded_Allo'], freqs=freqs, n_cycles=n_cycles,
                                     picks = pick_orig_hip, return_itc=False)
 raw_original_vr.copy().pick_channels(['SEEG_47'])
-power_onset_orig_hip_vr = tfr_morlet(epochs_original_vr['onsets_500_1500'], freqs=freqs, n_cycles=n_cycles,
+
+power_onset_orig_hip_vr = tfr_morlet(epochs_original_vr['onsets_500_1500'], freqs=freqs, n_cycles = n_cycles,
                                     picks = pick_orig_hip, return_itc=False)
 power_stop_orig_hip_vr = tfr_morlet(epochs_original_vr['stops_500_1500'], freqs=freqs, n_cycles=n_cycles,
                                     picks = pick_orig_hip, return_itc=False)
 
-power_point_orig_hip_vr_ego.plot_topo(picks=plot_pick_orig_hip, baseline=(-2., -1.5), mode='logratio', layout=box)
-power_point_orig_hip_vr_allo.plot_topo(picks=plot_pick_orig_hip, baseline=(-2., -1.5), mode='logratio', layout=box)
+power_point_orig_hip_vr_ego.plot_topo(picks=plot_pick_orig_hip, baseline=(-3, -2), mode='logratio', layout=box)
+power_point_orig_hip_vr_allo.plot_topo(picks=plot_pick_orig_hip, baseline=(-3, -2), mode='logratio', layout=box)
 
-power_onset_orig_hip_vr.plot_topo(picks=plot_pick_orig_hip, baseline=(-2., -1.5), mode='logratio', layout=box)
-power_stop_orig_hip_vr.plot_topo(picks=plot_pick_orig_hip, baseline=(-2., -1.5), mode='logratio', layout=box)
+power_onset_orig_hip_vr.plot_topo(picks=plot_pick_orig_hip, baseline=(-3., -2), mode='logratio', layout=box)
+power_stop_orig_hip_vr.plot_topo(picks=plot_pick_orig_hip, baseline=(-3, -2), mode='logratio', layout=box)
 
 power_trials_point_orig_hip_ego = tfr_morlet(epochs_original_vr['pointingEnded_Ego'], freqs=freqs, n_cycles=n_cycles,
                                     picks = pick_orig_hip, return_itc=False, average = False)
