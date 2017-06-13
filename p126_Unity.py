782cd90ef6f886d2bdb65d383b9065018921dfc8
hejtmy <hejtmy@gmail.com>
2017-06-13 02:12:17 +0200

    some additions

--- p126_Unity.py
+++ p126_Unity.py
@@ -10,8 +10,8 @@ from functions import read_eeg as readeegr
 from mne.stats import permutation_cluster_test
 from mne.time_frequency import psd_multitaper, tfr_multitaper, tfr_stockwell, tfr_morlet
 
-base_path = "D:\\IntracranialElectrodes\\Data\\p126\\"
-#base_path = "U:\\OneDrive\\FGU\\iEEG\\p126\\"
+#base_path = "D:\\IntracranialElectrodes\\Data\\p126\\"
+base_path = "U:\\OneDrive\\FGU\\iEEG\\p126\\"
 
 path_original_vr = base_path + "UnityAlloEgo\\EEG\\Preprocessed\\prep_250.mat"
 path_bip_vr = base_path + "UnityAlloEgo\\EEG\\Preprocessed\\prep_bipolar_250.mat"
@@ -40,22 +40,32 @@ raw_original_vr.plot(events = mne_events_vr, scalings='auto')
 raw_original_vr.info["bads"] = ['SEEG_55', 'SEEG_56', 'SEEG_57', 'SEEG_58', 'SEEG_59']
 
 ## Epoching
-epochs_original_vr = mne.Epochs(raw_original_vr, mne_events_vr, event_id=mapp_vr, tmin=-3, tmax=3, add_eeg_ref=False)
-epochs_bip_vr = mne.Epochs(raw_bip_vr, mne_events_vr, event_id=mapp_vr, tmin=-3, tmax=3, add_eeg_ref=False)
+epochs_original_vr = mne.Epochs(raw_original_vr, mne_events_vr, event_id=mapp_vr, tmin=-3, tmax=3, add_eeg_ref=False, baseline=None)
+#epochs_bip_vr = mne.Epochs(raw_bip_vr, mne_events_vr, event_id=mapp_vr, tmin=-3, tmax=3, add_eeg_ref=False, baseline=None)
 
 epochs_original_vr['onsets_500_1500', 'stops_500_1500'].plot(block=True, scalings='auto')
-epochs_bip_vr['onsets_500_1500', 'stops_500_1500'].plot(block=True, scalings='auto')
+#epochs_bip_vr['onsets_500_1500', 'stops_500_1500'].plot(block=True, scalings='auto')
 
 epochs_original_vr['onsets_500_1500', 'stops_500_1500'].plot(block=True, scalings='auto')
 
 freqs = np.arange(2, 30, 1)
 n_cycles = freqs / 2
 
-picks_original = mnehelp.def_picks(epochs_original_vr)
+picks_original = mnehelp.picks_all(epochs_original_vr)
+picks_hi = mnehelp.picks_all_localised(epochs_original_vr, pd_montage, 'Hi')
 box = mnehelp.custom_box_layout(picks_original, 8)
 plot_picks_perhead = range(0, len(picks_original))
 
-power_onset_original_vr = tfr_morlet(epochs_original_vr['onsets_500_1500'], freqs=freqs, n_cycles=n_cycles,
+power_original_point_ego_vr = tfr_morlet(epochs_original_vr['pointingEnded_Ego'], freqs=freqs, n_cycles=n_cycles,
                                     picks=picks_original, return_itc=False)
+
 # NEED to pass picks because default IGNORES SEEG channels
-power_onset_original_vr.plot_topo(picks=plot_picks_perhead, baseline=(-2., -1.5), mode='logratio', layout=box)
+power_original_point_ego_vr.plot_topo(picks=plot_picks_perhead, baseline=(-3, -2), mode='logratio', layout=box)
+
+conditions = ["pointingEnded_Ego", "pointingEnded_Allo"]
+evoked_dict = dict()
+for condition in conditions:
+    evoked_dict[condition] = epochs_original_vr[condition].average()
+
+colors = dict(pointingEnded_Ego="Crimson", pointingEnded_Allo="CornFlowerBlue")
+mne.viz.plot_compare_evokeds(evoked_dict, picks=picks_hi, colors=colors)
