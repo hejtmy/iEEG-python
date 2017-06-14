e5f4d0de71aa4eedd1591d39b5a4585eeefa5096
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-06-14 16:48:20 +0200

    Adds hleper function to generate random matplotlib rgb

--- functions/helpers.py
+++ functions/helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def remove_unnamed(pd_dataframe):
     cols = [c for c in pd_dataframe.columns if 'Unnamed' not in c]
@@ -8,3 +9,8 @@ def remove_unnamed(pd_dataframe):
 def find_duplicates(arr):
     vals, ids, count = np.unique(arr, return_index=True, return_counts=True)
     return ids[count > 1]
+
+def random_matplotlib_color():
+    r = lambda: random.randint(0,255)
+    rgb = (r()/255, r()/255,r()/255)
+    return tuple(rgb)
