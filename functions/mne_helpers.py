345d064880d987fcbde07bf624b824ad8e5a02cf
hejtmy <hejtmy@gmail.com>
2017-06-25 23:18:01 +0200

    better power spectru mestimate graphs

--- functions/mne_helpers.py
+++ functions/mne_helpers.py
@@ -19,6 +19,12 @@ def picks_localised(pd_montage, name):
     return pd_montage[name == pd_montage.neurologyLabel].index
 
 
+##Droping epochs helpers
+def get_dropped_epoch_indices(ls):
+    rm_ignored = [item for item in ls if item != ['IGNORED']]
+    drop_indices = [i for i, x in enumerate(rm_ignored) if x == ['USER']]
+    return(drop_indices)
+
 # Creates box layout for non standard topological images (such as seeg)
 # NEEDS channel names, unfortunately
 def custom_box_layout(ch_names, ncol = 6):
@@ -39,8 +45,8 @@ def custom_box_layout(ch_names, ncol = 6):
 
 
 ## PLOTS
-def plot_theta_epochs(epochs, picks, names):
-    plot_psd_epochs(epochs, picks, names, 1, 8)
+def plot_theta_epochs(epochs, picks, pick_names = [], event_names = []):
+    plot_psd_epochs(epochs, picks, 1, 8, pick_names = pick_names, event_names = event_names)
 
 
 def plot_theta_epochs_separate(epochs, picks, names):
@@ -54,10 +60,10 @@ def plot_psd_epochs_separate(epochs, picks, names, fmin, fmax):
     for idx, pick in enumerate(picks):
         linestyle = helpers.int_to_linestyle(idx)
         for channel in pick:
-            color = helpers.random_matplotlib_color();
+            color = helpers.random_matplotlib_color()
             print(channel)
             epochs.plot_psd(fmin = fmin, fmax = fmax,
-                      picks = [channel], color=color, show = False, ax = ax)    
+                      picks = [channel], color = color, show = False, ax = ax)
             ax.lines[-1].set_linestyle(linestyle)
             channel_name = "%s-%s" % (names[idx], str(channel))
             pick_names.append(channel_name)
@@ -65,12 +71,27 @@ def plot_psd_epochs_separate(epochs, picks, names, fmin, fmax):
     plt.legend(ax.lines, pick_names)
 
 
-def plot_psd_epochs(epochs, picks, names, fmin, fmax):
+def plot_psd_epochs(epochs, picks, fmin, fmax, pick_names = [], event_names = []):
     plt.figure()
     ax = plt.axes()
-    for idx, pick in enumerate(picks):
-        epochs.plot_psd(fmin = fmin, fmax = fmax,
-                      picks = pick, color = helpers.random_matplotlib_color(), 
-                      show = False, ax = ax)
+    final_names = []
+    pick_colors = [helpers.random_matplotlib_color() for _ in range(len(picks))]
+    event_linestyles = [helpers.int_to_linestyle(i) for i in range(len(epochs))]
+    for i, event_epoch in enumerate(epochs):
+        if (len(event_names) - 1 >= i):
+            event_name = event_names[i]
+        else: 
+            event_name = 'event' + str(i)
+        for idx, pick in enumerate(picks):
+            event_epoch.plot_psd(fmin = fmin, fmax = fmax,
+                          picks = pick, color = pick_colors[idx],
+                          show = False, ax = ax)
+            ax.lines[-1].set_linestyle(event_linestyles[i])
+            # creates line name
+            if (len(pick_names) - 1 >= idx):
+                pick_name = pick_names[idx]
+            else: 
+                pick_name = 'electrodes' + str(i)
+            final_names.append(pick_name + ": " + event_name)
     ax.set_title('Plot')
-    plt.legend(ax.lines, names)
+    plt.legend(ax.lines, final_names)
