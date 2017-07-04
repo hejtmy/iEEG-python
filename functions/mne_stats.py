2e0ef3db6948b5711a1a58ea4dcf55d1161f8176
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-07-04 17:59:02 +0200

    better stats

--- functions/mne_stats.py
+++ functions/mne_stats.py
@@ -1,14 +1,18 @@
 from scipy.stats import ttest_1samp, wilcoxon, ranksums, ttest_ind, mannwhitneyu
 import numpy as np
 import matplotlib.pyplot as plt
+from functions import mne_helpers as mnehelp
+from functions import helpers
 
 # TFR is event X electrode X freqs X time
+# Returns table and sued frequencies for laters
+# Wilcox table is Channel X time X freqs
 def wilcox_tfr_power(tfr1, tfr2):
-    events1 = reverse_tfr_list(tfr1.data)
-    events2 = reverse_tfr_list(tfr2.data)
+    events1 = mnehelp.reverse_tfr_list(tfr1.data)
+    events2 = mnehelp.reverse_tfr_list(tfr2.data)
     freqs = tfr2.freqs
     # reverse creates channel x freq X time X trials
-    wilcox_table = instantiate_zero_list(events1)
+    wilcox_table = mnehelp.instantiate_tfr_zero_list(events1)
     print("=" * len(wilcox_table))
     for i_ch, channel in enumerate(events1):
         print("=", end = "")
@@ -17,24 +21,11 @@ def wilcox_tfr_power(tfr1, tfr2):
                 time_res = ranksums(events1[i_ch][i_freq][i_time], events2[i_ch][i_freq][i_time])
                 # returns positive p values and negative p values
                 wilcox_table[i_ch][i_freq][i_time] = time_res.pvalue * (time_res.statistic/abs(time_res.statistic)) # converts statistic to -+ 1 depending on the direction
+    wilcox_table = np.asarray(wilcox_table)
     return wilcox_table, freqs
-    
-
-# reverses to Channel X Frequency X Time X Events
-def reverse_tfr_list(ls):
-    rev_ls = np.swapaxes(ls, 0, 3)
-    rev_ls = np.swapaxes(rev_ls, 0, 2)
-    rev_ls = np.swapaxes(rev_ls, 1, 0)
-    return(rev_ls)
-
-    
-# returns 0s given by length of first three elements of list for Wilcox Channel X Frequency X time
-def instantiate_zero_list(ls):
-    a, b, c = len(ls), len(ls[0]), len(ls[0][0])
-    zeros = [[[0 for x in range(c)] for y in range(b)] for z in range(a)]
-    return(zeros)
 
 
+# Wilcox table is in createschannel X time x freq
 def plot_wilcox(wilcox_table, channel, sampling_frequency, cutout = 0.05, freqs = []):
     if len(freqs) == 0:
         freqs = range(len(wilcox_table[channel]))
@@ -50,4 +41,26 @@ def plot_wilcox(wilcox_table, channel, sampling_frequency, cutout = 0.05, freqs 
     plt.figure()
     plt.pcolormesh(x, y, p_values)
     plt.colorbar()
+    plt.show()
+    
+    
+# Explanation
+def plot_wilcox_box(wilcox_table, sampling_frequency, cutout = 0.05, freqs = [], channels = []):
+    if len(freqs) == 0:
+        freqs = range(len(wilcox_table[0]) + 1)
+    if len(channels) == 0:
+        channels = range(len(wilcox_table))
+    times = range(len(wilcox_table[0][0]))
+    times = [time/sampling_frequency for time in times]
+    x, y = np.meshgrid(times, freqs)
+    nrow, ncol = helpers.nrow_ncol(len(channels))
+    fig, axes = plt.subplots(nrows=nrow, ncols=ncol)
+    for i_channel in channels:
+        #recalculates to seconds from sampling freq
+        p_values = np.array(wilcox_table[i_channel])
+        p_values[abs(p_values) > cutout] = 1
+        p_values[(0 < p_values) & (p_values <= cutout)] = 0.5
+        p_values[(cutout <= p_values) & (p_values < 0)] = -0.5
+        i_row, i_col = helpers.layout_position(i_channel, nrow, ncol)
+        axes[i_row, i_col].pcolormesh(x, y, p_values)
     plt.show()
