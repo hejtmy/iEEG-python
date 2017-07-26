757eaca6d667481e61a5063340635c76fd4a7c8d
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-07-26 19:00:42 +0200

    helper file for debuggin gpurposes

---
+++ debug.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 17:32:07 2017
+
+@author: Lukáš
+"""
+
+wilcox_ego_allo_lfo, wilcox_freqs = mnestats.wilcox_tfr_power(power_trial_point_perhead_hip_vr_ego_lfo, power_trial_point_perhead_hip_vr_allo_lfo)
