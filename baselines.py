71289f53113a21fa3a424975a50ccb89a237f80e
hejtmy <hejtmy@gmail.com>
2017-08-03 16:35:35 +0200

    defines new onset baseline

--- baselines.py
+++ baselines.py
@@ -4,12 +4,11 @@ Created on Mon Jul 31 12:37:59 2017
 
 @author: hejtm
 """
+power_onset_perhead_vr.apply_baseline(baseline = onset_baseline, mode = mode)
+power_stop_perhead_vr.apply_baseline(baseline = onset_baseline, mode = mode)
 
-power_onset_perhead_vr.apply_baseline(baseline = baseline, mode = mode)
-power_stop_perhead_vr.apply_baseline(baseline = baseline, mode = mode)
-
-power_trials_onset_perhead_vr.apply_baseline(baseline = baseline, mode = mode)
-power_trials_stop_perhead_vr.apply_baseline(baseline = baseline, mode = mode)
+power_trials_onset_perhead_vr.apply_baseline(baseline = onset_baseline, mode = mode)
+power_trials_stop_perhead_vr.apply_baseline(baseline = onset_baseline, mode = mode)
 
 power_point_perhead_vr_ego.apply_baseline(baseline = baseline, mode = mode)
 power_point_perhead_vr_allo.apply_baseline(baseline = baseline, mode = mode)
