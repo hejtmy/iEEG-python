c314ee3e31f5e92a1b278120b32e30c82ec74352
hejtmy <hejtmy@gmail.com>
2020-06-24 01:27:43 +0200

    Removes the pritns and debugs

--- functions/mne_visualisations.py
+++ functions/mne_visualisations.py
@@ -3,7 +3,9 @@ import numpy as np
 
 
 def plot_power_heatmap(tfr, ncol=6):
-    """[summary]
+    """Plots 
+
+    basically replaces plot topo power which for some reason I am quite unable to run 
 
     Parameters
     ----------
@@ -16,21 +18,16 @@ def plot_power_heatmap(tfr, ncol=6):
     if(nrow <= 1):
         Warning("Need to fix this for a single row")
         return
-    print(nrow, ncol)
     fig, axs = plt.subplots(ncols=ncol, nrows=nrow, sharex=True, sharey=True)
     # TODO - add 0 plotting
     x, xlabels = (0, len(tfr.times)), (tfr.times[0], tfr.times[-1])
     y, ylabels = range(0, len(tfr.freqs)), np.round(tfr.freqs, 3)
     for r in range(0, nrow):
         for c in range(0, ncol):
-            print(r, c)
             i = c + r*ncol
-            print(i)
             if i >= tfr.data.shape[0]:
                 break
-            print(axs)
             im = axs[r, c].pcolormesh(tfr.data[i, ...])
-            print("sasd")
             _ = axs[r, c].set_title(tfr.ch_names[i])
             _ = axs[r, c].set_xticks(x)
             _ = axs[r, c].set_xticklabels(xlabels)
